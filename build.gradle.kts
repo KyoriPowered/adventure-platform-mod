0f6732033b26d0f63d81fb8868a3c8febc110ef7
Zach Levis <zml@aoeu.xyz>
2020-04-13 22:27:20 -0700

    Switch to using centralized gradle plugin for project configuration
    plus a little bit more tidying

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,38 @@
+import ca.stellardrift.build.apiInclude
+import ca.stellardrift.build.implementationInclude
+import ca.stellardrift.build.kyoriText
 import net.fabricmc.loom.task.RemapJarTask
 import net.fabricmc.loom.task.RemapSourcesJarTask
 
 plugins {
-    `java-library`
-    `maven-publish`
-    id("com.github.hierynomus.license") version "0.15.0"
+    id("ca.stellardrift.opinionated") version "1.0.1"
     id("fabric-loom") version "0.2.7-SNAPSHOT"
 }
 
-group = "ca.stellardrift"
-version = "3.0.3-SNAPSHOT"
+val versionSelf = "1.0-SNAPSHOT"
+val versionMinecraft: String by project
+val versionText: String by project
+val versionMappings: String by project
+val versionLoader: String by project
+val versionFabricApi: String by project
 
+group = "ca.stellardrift"
+version = "$versionSelf+$versionText"
 
 repositories {
     jcenter()
     mavenCentral()
 }
 
-fun DependencyHandlerScope.apiInclude(dep: String, configure: ExternalModuleDependency.() -> Unit = {}) {
-    include(dep, configure)
-    modApi(dep, configure)
-}
-
-fun DependencyHandlerScope.implementationInclude(dep: String, configure: ExternalModuleDependency.() -> Unit = {}) {
-    include(dep, configure)
-    modImplementation(dep, configure)
-}
-
-val versionMinecraft = ext["version.minecraft"] as String
-val versionText = ext["version.text"] as String
-val versionMappings = ext["version.mappings"] as String
-val versionLoader = ext["version.loader"] as String
-val versionFabricApi = ext["version.fabricApi"] as String
 
 dependencies {
-    apiInclude("net.kyori:text-api:$versionText")
-    apiInclude("net.kyori:text-feature-pagination:$versionText")
-    apiInclude("net.kyori:text-serializer-plain:$versionText")
-    implementationInclude("net.kyori:text-serializer-gson:$versionText") {
+    apiInclude(kyoriText("api", versionText))
+    apiInclude(kyoriText("feature-pagination", versionText))
+    apiInclude(kyoriText("serializer-plain", versionText))
+
+    implementationInclude(kyoriText("serializer-gson", versionText)) {
         exclude("com.google.code.gson")
     }
-
 
     listOf("commands-v0", "api-base").forEach {
         implementationInclude("net.fabricmc.fabric-api:fabric-$it:$versionFabricApi")
@@ -49,13 +41,6 @@ dependencies {
     minecraft("com.mojang:minecraft:$versionMinecraft")
     mappings("net.fabricmc:yarn:$versionMinecraft+build.$versionMappings")
     modImplementation("net.fabricmc:fabric-loader:$versionLoader")
-
-
-    // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-
-    // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 minecraft {
@@ -63,67 +48,29 @@ minecraft {
 }
 
 license {
-    mapping("java", "SLASHSTAR_STYLE")
-    include("**/*.java")
+    header = rootProject.file("LICENSE")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks.withType(JavaCompile::class) {
-    options.encoding = "UTF-8"
-}
-
-tasks.withType(Javadoc::class) {
-    options {
-        this.encoding = "UTF-8"
-    }
-}
-
-tasks.processResources {
+tasks.processResources.configure {
     expand("project" to project)
 }
 
-tasks.withType(Test::class) {
-    // Use junit platform for unit tests
-    useJUnitPlatform()
-}
+opinionated {
+    github("PEXPlugins", "text-adapter-fabric")
+    mit()
+    publication.apply {
+        val remapJar by tasks.getting(RemapJarTask::class)
+        val remapSourcesJar by tasks.getting(RemapSourcesJarTask::class)
+        suppressAllPomMetadataWarnings()
 
-val remapJar by tasks.getting(RemapJarTask::class)
-val remapSourcesJar by tasks.getting(RemapSourcesJarTask::class)
-
-val sourcesJar by tasks.registering(Jar::class) {
-    dependsOn(tasks.classes)
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
-
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-    from(tasks.javadoc)
-}
-
-
-publishing {
-    publications {
-        register<MavenPublication>("mavenJava") {
-            suppressAllPomMetadataWarnings()
-
-            artifact(tasks.jar.get()) {
-                classifier = "dev"
-            }
-            artifact(remapJar)
-
-            artifact(sourcesJar.get()) {
-                builtBy(remapSourcesJar)
-            }
-            artifact(javadocJar.get())
+        artifact(tasks.jar.get()) {
+            classifier = "dev"
         }
-    }
-    repositories {
-        mavenLocal()
+        artifact(remapJar)
+
+        artifact(tasks.getByName("sourcesJar")) {
+            builtBy(remapSourcesJar)
+        }
+        artifact(tasks.getByName("javadocJar"))
     }
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,4 +17,5 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 rootProject.name = "text-adapter-fabric"
