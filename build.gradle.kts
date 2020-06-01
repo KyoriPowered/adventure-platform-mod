d7ae2249206513d632523ae33260e0ecdba8d4a5
Zach Levis <zml@aoeu.xyz>
2020-06-01 13:41:06 -0700

    Bump opinionated version to 3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,20 @@
 
-import ca.stellardrift.build.apiInclude
-import ca.stellardrift.build.implementationInclude
 import net.fabricmc.loom.task.RemapJarTask
 import net.fabricmc.loom.task.RemapSourcesJarTask
 
 plugins {
-    id("ca.stellardrift.opinionated") version "2.0"
-    id("ca.stellardrift.opinionated.publish") version "2.0"
-    id("fabric-loom") version "0.4.3"
+    id("ca.stellardrift.opinionated.fabric") version "3.0"
+    id("ca.stellardrift.opinionated.publish") version "3.0"
 }
 
-val versionSelf = "1.0.1"
+val versionSelf = "2.0-SNAPSHOT"
 val versionMinecraft: String by project
 val versionAdventure: String by project
 val versionMappings: String by project
 val versionLoader: String by project
 
 group = "ca.stellardrift"
-version = "$versionSelf+$versionAdventure"
+version = "$versionSelf+${versionAdventure.replace("-SNAPSHOT", "")}"
 
 repositories {
     mavenLocal()
@@ -33,23 +30,19 @@ fun DependencyHandler.adventure(comp: String, version: Any): String {
 }
 
 dependencies {
-    apiInclude("net.kyori:examination-api:1.0.0-SNAPSHOT")
-    apiInclude("net.kyori:examination-string:1.0.0-SNAPSHOT")
-    apiInclude(adventure("api", versionAdventure))
-    apiInclude(adventure("text-feature-pagination", versionAdventure))
-    apiInclude(adventure("text-serializer-plain", versionAdventure))
+    api(include("net.kyori:examination-api:1.0.0-SNAPSHOT")!!)
+    api(include("net.kyori:examination-string:1.0.0-SNAPSHOT")!!)
+    api(include(adventure("api", versionAdventure))!!)
+    api(include(adventure("text-feature-pagination", versionAdventure))!!)
+    api(include(adventure("text-serializer-plain", versionAdventure))!!)
 
-    implementationInclude(adventure("text-serializer-gson", versionAdventure)) {
+    implementation(include(adventure("text-serializer-gson", versionAdventure)) {
         exclude("com.google.code.gson")
-    }
+    })
 
     minecraft("com.mojang:minecraft:$versionMinecraft")
     mappings("net.fabricmc:yarn:$versionMinecraft+build.$versionMappings:v2")
     modImplementation("net.fabricmc:fabric-loader:$versionLoader")
-}
-
-minecraft {
-    refmapName = "${rootProject.name.toLowerCase()}-refmap.json"
 }
 
 license {
@@ -58,20 +51,6 @@ license {
 
 tasks.processResources.configure {
     expand("project" to project)
-}
-
-bintray {
-    pkg.version.name = project.version as String
-    pkg.version.released = null
-}
-
-tasks.withType(Javadoc::class).configureEach {
-    val options = this.options
-    if (options is StandardJavadocDocletOptions) {
-        options.links(
-
-        )
-    }
 }
 
 opinionated {
