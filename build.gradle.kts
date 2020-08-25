25a0dba400ed239364e4601025ba98432e9476cd
Zach Levis <zml@aoeu.xyz>
2020-08-24 22:04:56 -0700

    Correctly include adventure in generated pom, and set up publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 
 import ca.stellardrift.build.common.adventure
+import ca.stellardrift.build.common.isRelease
 import ca.stellardrift.build.common.sonatypeOss
 
 plugins {
-  id("ca.stellardrift.opinionated.fabric") version "3.1"
+  id("fabric-loom") version "0.5-SNAPSHOT"
   id("ca.stellardrift.opinionated.publish") version "3.1"
+  id("ca.stellardrift.opinionated.fabric") version "3.1"
   checkstyle
 }
 
@@ -26,14 +28,16 @@ repositories {
 }
 
 dependencies {
-  api(include("net.kyori:examination-api:1.0.0-SNAPSHOT")!!)
-  api(include("net.kyori:examination-string:1.0.0-SNAPSHOT")!!)
-  api(include(adventure("api", versionAdventure))!!)
-  api(include(adventure("text-feature-pagination", versionAdventure))!!)
-  api(include(adventure("text-serializer-plain", versionAdventure))!!)
-  api(include(adventure("platform-api", versionAdventure))!!)
+  modApi(include("net.kyori:examination-api:1.0.0-SNAPSHOT")!!)
+  modApi(include("net.kyori:examination-string:1.0.0-SNAPSHOT")!!)
+  modApi(include(adventure("api", versionAdventure))!!)
+  modApi(include(adventure("text-feature-pagination", versionAdventure))!!)
+  modApi(include(adventure("text-serializer-plain", versionAdventure))!!)
+  modApi(include(adventure("platform-api", versionAdventure)) {
+    exclude("com.google.code.gson")
+  })
 
-  implementation(include(adventure("text-serializer-gson", versionAdventure)) {
+  modImplementation(include(adventure("text-serializer-gson", versionAdventure)) {
       exclude("com.google.code.gson")
   })
   modImplementation("ca.stellardrift:colonel:0.1")
@@ -65,4 +69,38 @@ opinionated {
   github("KyoriPowered", "adventure-platform-fabric")
   mit()
   useJUnit5()
+  publication?.apply {
+    pom {
+      url.set("https://github.com/KyoriPowered/adventure")
+
+      developers {
+        developer {
+          id.set("kashike")
+          timezone.set("America/Vancouver")
+        }
+
+        developer {
+          id.set("lucko")
+          name.set("Luck")
+          url.set("https://lucko.me")
+          email.set("git@lucko.me")
+        }
+
+        developer {
+          id.set("zml")
+          name.set("zml")
+          timezone.set("America/Vancouver")
+        }
+
+        developer {
+          id.set("Electroid")
+        }
+      }
+    }
+  }
+  if (isRelease()) {
+    publishTo("ossrh", "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+  } else {
+    publishTo("ossrh", "https://oss.sonatype.org/content/repositories/snapshots/")
+  }
 }
