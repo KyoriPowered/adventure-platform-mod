d9d9a4de211555c3c3d0400eb0c4631af02af54d
Zach Levis <zml@aoeu.xyz>
2020-08-26 02:44:32 -0700

    Get more info on GH actions build failure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@
 pluginManagement {
     repositories {
         mavenLocal()
-        maven("https://oss.sonatype.org/content/groups/public/")
         jcenter()
         maven {
             name = "Fabric"
