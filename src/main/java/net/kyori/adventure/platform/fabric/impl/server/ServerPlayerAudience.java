b6934c9d4da026ac2d23ac2c26d32863dbd14fcd
Zach Levis <zml@aoeu.xyz>
2020-11-11 13:08:32 -0800

    server: Fix sending books

--- src/main/java/net/kyori/adventure/platform/fabric/impl/server/ServerPlayerAudience.java
+++ src/main/java/net/kyori/adventure/platform/fabric/impl/server/ServerPlayerAudience.java
@@ -152,7 +152,7 @@ public final class ServerPlayerAudience implements Audience {
   }
 
   private String adventure$serialize(final @NonNull Component component) {
-    final Locale locale = ((ConnectionAccess) this.player.connection).getChannel().attr(FriendlyByteBufBridge.CHANNEL_LOCALE).get();
+    final Locale locale = ((ConnectionAccess) this.player.connection.getConnection()).getChannel().attr(FriendlyByteBufBridge.CHANNEL_LOCALE).get();
     return FabricAudiences.gsonSerializer().serialize(this.controller.localeRenderer().render(component, locale == null ? Locale.getDefault() : locale));
   }
 
