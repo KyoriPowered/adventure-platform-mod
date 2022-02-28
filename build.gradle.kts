d518b6eb5476cd1964deeed9dcdc0e8c37c26e5b
zml <zml@stellardrift.ca>
2022-02-28 14:48:37 -0800

    release: Version 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ val versionMinecraft: String by project
 val versionParchment: String by project
 
 group = "net.kyori"
-version = "5.1.0-SNAPSHOT"
+version = "5.1.0"
 description = "Integration between the adventure library and Minecraft: Java Edition, using the Fabric modding system"
 
 repositories {
