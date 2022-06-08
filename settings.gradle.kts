66722cd3b9ba5a41ea7919b6711bade8c3678c87
zml <zml@stellardrift.ca>
2022-06-07 21:17:58 -0700

    build: use my repo instead?

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,17 @@
 pluginManagement {
   repositories {
+    maven(url = "https://repo.stellardrift.ca/repository/internal/") {
+      name = "stellardriftReleases"
+      mavenContent { releasesOnly() }
+    }
+    maven(url = "https://repo.stellardrift.ca/repository/snapshots/") {
+      name = "stellardriftSnapshots"
+      mavenContent { snapshotsOnly() }
+    }
     mavenCentral()
     maven {
       name = "Fabric"
       url = uri("https://maven.fabricmc.net")
-    }
-    maven {
-      name = "Cotton"
-      url = uri("https://server.bbkr.space/artifactory/libs-release/")
     }
     gradlePluginPortal()
   }
