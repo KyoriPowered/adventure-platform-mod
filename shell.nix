8f95e2ff88679a691b02fabaa6893f30667bc4a8
zml <zml@stellardrift.ca>
2024-01-20 15:44:49 -0800

    chore: remove old unused build files

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{pkgs ? import <nixpkgs> {}, jdk ? pkgs.jdk12 }:
-let 
-  gradle6 = pkgs.callPackage gradle/gradle.nix { java = jdk; };
-in
-pkgs.stdenv.mkDerivation {
-  name = "configurate";
-  buildInputs = with pkgs; [ jdk gradle6 git bash ];
-}
