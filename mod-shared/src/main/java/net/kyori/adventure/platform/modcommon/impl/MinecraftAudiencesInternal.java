5d17f3b3c02104b67ca183d663d9a6d7b37e099e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-07-24 13:43:21 -0700

    remove redundant overrides

--- mod-shared/src/main/java/net/kyori/adventure/platform/modcommon/impl/MinecraftAudiencesInternal.java
+++ mod-shared/src/main/java/net/kyori/adventure/platform/modcommon/impl/MinecraftAudiencesInternal.java
@@ -24,16 +24,9 @@
 package net.kyori.adventure.platform.modcommon.impl;
 
 import net.kyori.adventure.platform.modcommon.MinecraftAudiences;
-import net.kyori.adventure.pointer.Pointered;
-import net.kyori.adventure.text.renderer.ComponentRenderer;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
 public interface MinecraftAudiencesInternal extends MinecraftAudiences {
   @NotNull RegistryAccess registryAccess();
-
-  @NotNull ComponentRenderer<Pointered> renderer();
-
-  Component toNative(net.kyori.adventure.text.Component adventure);
 }
