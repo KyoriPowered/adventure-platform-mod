033526085dd0084448179d60acc8033c6ebeadb4
Yang Seong Mo <angryko@naver.com>
2024-12-04 03:13:57 +0900

    Bump to 1.21.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-  id("fabric-loom") version "1.8.12"
+  id("fabric-loom") version "1.9.2"
   id("net.neoforged.moddev.repositories") version "1.0.21"
 }
 
